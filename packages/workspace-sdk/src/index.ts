978c90622e411f14728d4569afa5ff31dfefe0b9
Newman Chow <newman@oursky.com>
2026-04-01 19:24:26 +0000

    refactor: remove legacy workspace package paths

--- packages/workspace-sdk/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { WorkspaceClient } from './client';
-export { FSOperations } from './fs';
-export { ExecOperations } from './exec';
-export { WorkspaceManager } from './workspace-manager';
-export { WorkspaceHandle } from './workspace-handle';
-export { SpotlightOperations } from './spotlight';
-
-export * from './types';
